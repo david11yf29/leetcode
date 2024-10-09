77838c3df3bb420751e7793e100522eeecdb8d7f
David Lin <david11yf29@gmail.com>
2024-10-09 17:38:18 +0800

    2024.10.09 LeetCode

---
+++ src/main/java/com/example/algorithm/binarysearch.java
@@ -0,0 +1,22 @@
+package com.example.algorithm;
+
+public class binarysearch {
+	/// # 二分查找基礎版<br/>
+	/// <br/>
+	/// Params:<br/>
+	/// - arr: 待查找的升序数组<br/>
+	/// - target: 待找的目标值<br/>
+	/// <br/>
+	/// Returns:<br/>
+	/// - 找到则返回索引<br/>
+	/// - 找不到返回-1<br/>
+	public static int binarySearch(int[] arr, int target) {
+		int i = 0, j = arr.length - 1;
+		while (i <= j) {
+			int m = (i + j) / 2;
+			arr[m]
+		}
+		return -1;
+	}
+
+}
